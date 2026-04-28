c571120278222a1a33b6cb5cd87c454c236aa00a
sehaj0007 <kaursehajpreet0728@gmail.com>
2026-04-28 12:21:43 +0530

    updated app to Version 5

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("Hello from GitOps with Argo CD - Version 4 🚀");
+  res.end("Hello from GitOps with Argo CD - Version 5 🚀");
 }).listen(3000);
